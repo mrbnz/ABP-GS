c9ec70fea8e0976d623201828a340a04e386572a
Genis <gpm@tuta.com>
2024-11-28 16:39:49 +0100

    updateLocal

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
-mb_internal_encoding("UTF-8");
+if (function_exists('mb_internal_encoding')) {
+    mb_internal_encoding("UTF-8");
+}
 require './config/funcionsInicials.php';
 require './config/baseDeDades.php';
 $router = new RouterController();
