d6c422c2eaf1e61058fbd73fb8c246871bc48e6e
StefanMorosanCamara <stefanmorosanc@gmail.com>
2024-12-05 16:37:18 +0100

    Seguim amb els filtres

---
+++ bundle/TipusActivitat/models/TipusActivitatManager.php
@@ -0,0 +1,37 @@
+<?php
+class TipusActivitatManager extends TipusActivitat {
+    public function selectAll() {
+		$resposta = null;
+			try {
+				$consulta = (BdD::$connection)->prepare(
+					'SELECT * FROM `tipus_activitat`'
+					);
+				$qFiles = $consulta->execute();
+				if ($qFiles > 0) {
+					$consulta->setFetchMode(PDO::FETCH_ASSOC); 
+					$resposta = $consulta->fetchAll();
+				}
+			} catch(PDOException $e) {
+				echo "***Error***: " . $e->getMessage();
+			}
+		return $resposta;
+	}
+
+    public function selectAllNom() {
+		$resposta = null;
+			try {
+				$consulta = (BdD::$connection)->prepare(
+					'SELECT nom FROM `tipus_activitat`'
+					);
+				$qFiles = $consulta->execute();
+				if ($qFiles > 0) {
+					$consulta->setFetchMode(PDO::FETCH_ASSOC); 
+					$resposta = $consulta->fetchAll();
+				}
+			} catch(PDOException $e) {
+				echo "***Error***: " . $e->getMessage();
+			}
+		return $resposta;
+	}
+}
+?>
