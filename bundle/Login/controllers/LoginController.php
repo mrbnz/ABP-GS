dd6a8976280bd4729da5d24c002ee0bad956a47d
Genis <gpm@tuta.com>
2024-12-07 20:16:54 +0100

    arreglat editUser no agafava be dataNaixa, corregix update, afegit eliminarUsuari amb id user, i veure llistat d'users

--- bundle/Login/controllers/LoginController.php
+++ bundle/Login/controllers/LoginController.php
@@ -8,17 +8,17 @@ class LoginController extends Controller
 {
 	public function process($params)
 	{
-		error_log("Process method called in LoginController");
+		//echo "S'ha cridat el mètode process a LoginController<br>";
 
 		// Si hem cridat a Logout
 		if (isset($params[0]) && ($params[0] == "logout")) {
-			error_log("Logout called");
+			echo "S'ha cridat Logout<br>";
 			$this->logout();
 		}
 
 		// Si estem logats ja
 		if ($this->EstaLogat()) {
-			error_log("User is already logged in");
+			echo "L'usuari ja està logat<br>";
 			if (isset($_SESSION['username'])) {
 				echo "<script>console.log('Ja estàs logat com a " . $_SESSION['username'] . "');</script>";
 			} else {
@@ -30,25 +30,25 @@ class LoginController extends Controller
 				$nom_usuari = isset($_POST["usuari"]) ? trim($_POST["usuari"]) : null;
 				$contrasenya = isset($_POST["contrasenya"]) ? $_POST["contrasenya"] : null;
 
-				error_log("Login attempt for user: " . $nom_usuari);
 
 				if ($nom_usuari && $contrasenya) {
 					$UsuariMng = new UsuariManager();
 					if ($UsuariMng->verificar($nom_usuari, $contrasenya)) {
-						error_log("Login successful for user: " . $nom_usuari);
+						echo "Login exitós per l'usuari: " . $nom_usuari . "<br>";
 						$this->login($nom_usuari);
 						$this->redirect("");
 					} else {
-						error_log("Login failed for user: " . $nom_usuari);
+						echo "Login fallit per l'usuari: " . $nom_usuari . "<br>";
 						$this->data["missatge"] = "Usuari o contrasenya incorrecte!";
 						$this->twig = "login.html";
 					}
 				} else {
-					error_log("Missing data for login");
+					echo "Falten dades per al login<br>";
 					$this->data["missatge"] = "Falten dades!";
 					$this->twig = "login.html";
 				}
 			} else {
+				echo "User no logat, enviem a login<br>";
 				$this->twig = "login.html";
 			}
 		}
@@ -56,9 +56,9 @@ class LoginController extends Controller
 
 	public function logout()
 	{
+		echo "S'ha cridat el mètode logout<br>";
 		$UsuariMng = new UsuariManager();
 		$UsuariMng->tancar();
 		$this->redirect("hola");
 	}
 }
-?>
