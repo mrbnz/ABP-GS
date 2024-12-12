17ddd0ecc3ef2ed471f13c443f68eb0012f4f782
Genis <gpm@tuta.com>
2024-12-12 17:17:48 +0100

    millora, activitat a boostrap, afegit fotos, millora sql, millora detall

--- bundle/Activitat/models/ActivitatManager.php
+++ bundle/Activitat/models/ActivitatManager.php
@@ -34,31 +34,31 @@ class ActivitatManager extends Activitat {
     {
         try {
             $consulta = (BdD::$connection)->prepare('
-                SELECT 
-					a.id, 
-					a.nom, 
-					a.descripcio_breu, 
-					a.descripcio, 
-					a.data, 
-					a.places_totals, 
-					a.places_ocupades, 
-					a.preu,
-                    a.id_tipus_activitat 
-					e.nom AS espai_nom, 
-					e.ubicacio AS espai_ubicacio, 
-					e.capacitat AS espai_capacitat, 
-					e.descripcio AS espai_descripcio,
-					m.ruta AS imatge
-				FROM 
-					activitat a
-				JOIN 
-					espai e ON a.id_espai = e.id
-				LEFT JOIN 
-					activitat_multimedia am ON a.id = am.id_activitat
-				LEFT JOIN 
-					multimedia m ON am.id_multimedia = m.id
-				WHERE 
-					a.id = :id
+               SELECT 
+                a.id, 
+                a.nom, 
+                a.descripcio_breu, 
+                a.descripcio, 
+                a.data, 
+                a.places_totals, 
+                a.preu, 
+                a.id_espai, 
+                a.id_tipus_activitat, 
+                e.nom AS espai_nom, 
+                e.ubicacio AS espai_ubicacio, 
+                e.capacitat AS espai_capacitat, 
+                e.descripcio AS espai_descripcio,
+                m.ruta AS imatge
+            FROM 
+                activitat a
+            JOIN 
+                espai e ON a.id_espai = e.id
+            LEFT JOIN 
+                activitat_multimedia am ON a.id = am.id_activitat
+            LEFT JOIN 
+                multimedia m ON am.id_multimedia = m.id
+            WHERE 
+                a.id = :id
             ');
             $consulta->bindValue(':id', $id, PDO::PARAM_INT);
             $consulta->execute();
@@ -72,13 +72,31 @@ class ActivitatManager extends Activitat {
             return null;
         }
     }
-    public function addActivitat($nom, $descripcioBreu, $descripcio, $data, $placesTotals, $preu, $idEspai, $idOrganitzador)
+    public function addActivitat($nom, $descripcioBreu, $descripcio, $data, $placesTotals, $preu, $idEspai, $idOrganitzador, $idTipusActivitat)
     {
         try {
             $consulta = (BdD::$connection)->prepare('
-                INSERT INTO activitat (nom, descripcio_breu, descripcio, data, places_totals, preu, id_espai)
-                VALUES (:nom, :descripcio_breu, :descripcio, :data, :places_totals, :preu, :id_espai)
+                INSERT INTO activitat (
+                    nom, 
+                    descripcio_breu, 
+                    descripcio, 
+                    data, 
+                    places_totals, 
+                    preu, 
+                    id_espai,
+                    id_tipus_activitat
+                ) VALUES (
+                    :nom,
+                    :descripcio_breu,
+                    :descripcio,
+                    :data,
+                    :places_totals,
+                    :preu,
+                    :id_espai,
+                    :id_tipus_activitat
+                )
             ');
+            
             $consulta->bindValue(':nom', $nom, PDO::PARAM_STR);
             $consulta->bindValue(':descripcio_breu', $descripcioBreu, PDO::PARAM_STR);
             $consulta->bindValue(':descripcio', $descripcio, PDO::PARAM_STR);
@@ -86,12 +104,15 @@ class ActivitatManager extends Activitat {
             $consulta->bindValue(':places_totals', $placesTotals, PDO::PARAM_INT);
             $consulta->bindValue(':preu', $preu, PDO::PARAM_STR);
             $consulta->bindValue(':id_espai', $idEspai, PDO::PARAM_INT);
-            $consulta->execute();
-            return BdD::$connection->lastInsertId();
-        } catch (PDOException $e) {
+            $consulta->bindValue(':id_tipus_activitat', $idTipusActivitat, PDO::PARAM_INT);
+            
+            if ($consulta->execute()) {
+                return (BdD::$connection)->lastInsertId();
+            }
+        } catch(PDOException $e) {
             echo "***Error***: " . $e->getMessage();
-            return false;
         }
+        return false;
     }
     public function addActivitatMultimedia($idActivitat, $idMultimedia)
     {
@@ -148,13 +169,18 @@ class ActivitatManager extends Activitat {
                 e.nom AS espai_nom, 
                 e.ubicacio AS espai_ubicacio, 
                 e.capacitat AS espai_capacitat, 
-                e.descripcio AS espai_descripcio
+                e.descripcio AS espai_descripcio,
+                m.ruta AS imatge
             FROM 
                 activitat a
             JOIN 
                 espai e ON a.id_espai = e.id
             JOIN 
                 tipus_activitat t ON a.id_tipus_activitat = t.id
+            LEFT JOIN 
+                activitat_multimedia am ON a.id = am.id_activitat
+            LEFT JOIN 
+                multimedia m ON am.id_multimedia = m.id 
         ');
         
         
